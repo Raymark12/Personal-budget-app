982c923dbfe1e88a609067c1bed269f8e27a6224
“Ivan <ivar.rf.96@gmail.com>
2021-11-23 20:45:10 -0300

    adds operations methods and controllers

--- controllers/operation.js
+++ controllers/operation.js
@@ -1,3 +1,15 @@
 exports.getOperations = (req, res, next) => {
     res.send('Operations');
 };
+
+exports.addOperation = (req, res, next) => {
+    res.send('Add Operation');
+};
+
+exports.updateOperation = (req, res, next) => {
+    res.send('Update Operation');
+};
+
+exports.deleteOperation = (req, res, next) => {
+    res.send('Delete Operation');
+};
--- routes/operation.js
+++ routes/operation.js
@@ -4,6 +4,13 @@ const operationController = require('../controllers/operation');
 
 const router = express.Router();
 
-router.get('/list', operationController.getOperations);
+router.get('/', operationController.getOperations);
+
+router.post('/', operationController.addOperation);
+
+router.patch('/', operationController.updateOperation);
+
+router.delete('/', operationController.deleteOperation);
+
 
 module.exports = router;
