13e1d97422af117ee749ffdc66004c391ac58405
“Ivan <ivar.rf.96@gmail.com>
2021-11-23 23:55:20 -0300

    fix update operation response status code

--- controllers/operation.js
+++ controllers/operation.js
@@ -29,7 +29,7 @@ exports.updateOperation = (req, res, next) => {
             operation.concept = concept;
             operation.amount = amount;
             operation.save();
-            res.status(201).json({
+            res.status(200).json({
                 message: 'Operation updated successfuly',
                 operation,
             })
