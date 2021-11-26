367139653474567eb33b6b9be3e1e6d40c9097e6
“Ivan <ivar.rf.96@gmail.com>
2021-11-26 18:32:47 -0300

    adds body to layout

--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,11 +1,13 @@
 import { Toolbar } from "../Navigation/Toolbar";
+import style from "./layout.module.scss";
 
-const Layout = () => {
-    return (
-        <div>
-            <Toolbar>Personal Budget</Toolbar>
-        </div>
-    );
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Toolbar>Personal Budget</Toolbar>
+      <div className={style.body} >{children}</div>
+    </div>
+  );
 };
 
 export default Layout;
