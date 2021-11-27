7ad265dc62fbdb63bbb5260943674092ac720ccf
“Ivan <ivar.rf.96@gmail.com>
2021-11-27 01:34:08 -0300

    implements navigation routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Layout from "./components/Layout";
 import Operations from "./components/Operations";
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Operations />
-      </Layout>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/operations" element={<Operations />} />
+            <Route path="/" element="Home" />
+          </Routes>
+        </Layout>
+      </Router>
     </div>
   );
 }
--- client/src/components/Layout/index.js
+++ client/src/components/Layout/index.js
@@ -1,11 +1,26 @@
+import { NavLink } from "react-router-dom";
 import Toolbar from "../Navigation/Toolbar";
 import style from "./layout.module.scss";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <Toolbar>Personal Budget</Toolbar>
-      <div className={style.body} >{children}</div>
+      <Toolbar>
+        <div>Personal Budget</div>
+        <nav>
+          <ul>
+            <li>
+              <NavLink exact={true} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/operations">Operations</NavLink>
+            </li>
+          </ul>
+        </nav>
+      </Toolbar>
+      <div className={style.body}>{children}</div>
     </div>
   );
 };
